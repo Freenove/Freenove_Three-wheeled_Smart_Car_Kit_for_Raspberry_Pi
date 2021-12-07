1e91643458191f7c7127d10c4aec944e4144b402
DenzelChen <291689742@qq.com>
2021-12-07 13:51:23 +0800

    Update Start_mjpg_Streamer.sh

--- Server/Start_mjpg_Streamer.sh
+++ Server/Start_mjpg_Streamer.sh
@@ -4,5 +4,5 @@ curr_file=$0
 cd "$(dirname "$curr_file")"
 cd ../mjpg-streamer
 #pwd
-./mjpg_streamer -i "./input_uvc.so -y -d /dev/video0 -n -r 320*240 -f 30" -o "./output_http.so -p 8090 -w ./www"
+./mjpg_streamer -i "./input_uvc.so -y -d /dev/video0 -n -r 320x240 -f 30" -o "./output_http.so -p 8090 -w ./www"
 
